31ff0392daa6dbe5d4f94105a00806e5f7bfd53c
kanocarra <emily.melhuish@gmail.com>
2016-09-22 19:19:26 +1200

    Did I even add

---
+++ Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+communications.c
+
+power.c
+
+pwm.c
+
+smart-fan-controller.c
+
+speed-control.c
+
